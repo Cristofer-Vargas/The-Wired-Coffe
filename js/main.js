b5fa73ac70d5bafa63c41ec5b87cf40d755a91ef
Cristofer Vargas de Santis <84339046+Cristofer-Vargas@users.noreply.github.com>
2023-02-22 21:47:02 -0300

    Usando js para evitar códigos repetidos and att
    Foi trocado o método de incorporar um código html de outro arquivo com PHP para JS

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const headerTagHTML = document.getElementById('recicly-code')
+
+fetch('../header.html')
+  .then(function (response) {
+    return response.text();
+  })
+  .then(function (conteudoHTML) {
+    // faça algo com o conteúdo HTML retornado
+    headerTagHTML.insertAdjacentHTML('beforeend', conteudoHTML)
+
+  });
+;
